5652162758470c1bb573c600910783202fb0c61f
Andika Kurniawan <dikzkeyboardfreak@gmail.com>
2016-05-16 10:21:22 +0700

    My Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,36 @@
 
 ## Write a short comment describing this function
 
+#This function create special matrix. 
+#It means the matrix not only has value but also other datas. 
+#This method has matrix value, matrix inverse value and other functions
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) inv <<- inverse
+  getInverse <- function() inv
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
-
 ## Write a short comment describing this function
-
+#This function create inverse on matrix from makeCacheMatrix method. 
+#It's created in 'inv <- solve(mat, ...)' then the inverse attached in 'x$setInverse(inv)'. 
+#And also this function check is matrix has inverse ? The function checks by 'if (!is.null(inv))'
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if (!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setInverse(inv)
+  inv
 }
